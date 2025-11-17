62fd55207f835712367f238c90e564b046e1dccf
Prem Kumar <prem144047@gmail.com>
2025-11-17 22:04:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.addEventListener("DOM is loaded"),
+	function () {
+		const messageEliment = document.createElement("p");
+		messageEliment.textContent = "DOM load success";
+
+		document.body.appendChield(messageEliment);
+	});
